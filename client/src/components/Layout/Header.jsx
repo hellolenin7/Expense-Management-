9f1107249e2a2c4659e3c61cc8ec85ac8d5ba70b
hellolenin7 <lenin3639@gmail.com>
2025-08-25 12:28:04 +0530

    new commit

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -1,7 +1,7 @@
 import React ,{useState,useEffect} from 'react'
 import { Link,useNavigate } from 'react-router-dom'
 import {message} from 'antd'
-import "../../styles/HeaderStyle.css";
+import "../../Styles/HeaderStyle.css";
 import { UserOutlined } from '@ant-design/icons';
 
 
