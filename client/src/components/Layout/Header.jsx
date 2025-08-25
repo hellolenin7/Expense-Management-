be6304ae8736d2e2407c96f5a8b75773f2359238
hellolenin7 <lenin3639@gmail.com>
2025-08-25 16:49:13 +0530

    new commit

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -20,31 +20,44 @@ const Header= () => {
       message.success("You are logout succesfully");
       navigate('/login')
     }
+    const [menuOpen, setMenuOpen] = useState(false);
     return(
         <>
           <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <Link className="navbar-brand" to="/">Income-Expense Management</Link>
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          {" "}
-             <h6 className="nav-link ">
-                    <UserOutlined />{loginUser && loginUser.name}
-              </h6>{" "}
-        </li>
-         <li className="nav-item">
-          <button className="btn btn-danger" onClick={logoutHandler}>
-            Logout
-            </button>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+      <div className="container-fluid">
+        {/* Brand - stays on the left always */}
+        <Link className="navbar-brand" to="/">
+          Income-Expense Management
+        </Link>
+
+        {/* Navbar toggler for small screens */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Menu items - username + logout */}
+        <div
+          className={`collapse navbar-collapse ${menuOpen ? "show" : ""}`}
+        >
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <h6 className="nav-link">
+                <UserOutlined /> {loginUser && loginUser.name}
+              </h6>
+            </li>
+            <li className="nav-item">
+              <button className="btn btn-danger" onClick={logoutHandler}>
+                Logout
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
         </>
     )
 }
